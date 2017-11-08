01a63c37b9c620b0c5a4947ab1d6242bfd41aef2
leticiarod <leticiamrgs@gmail.com>
2017-11-08 10:48:29 -0300

    Implemented home screen layout and added actions method to some views.

--- MovieNight/ViewController.swift
+++ MovieNight/ViewController.swift
@@ -10,18 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let client = MovieNightAPIClient()
-
-
+    
+    @IBOutlet weak var viewResultsButton: UIButton!
+    @IBOutlet weak var watcherOneBubbleImageView: UIImageView!
+    @IBOutlet weak var watcherTwoBubbleImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // settings to viewResultsButton
+        viewResultsButton.isEnabled = false
+        viewResultsButton.setTitleColor(UIColor.lightGray, for: .disabled)
+        
+        // adding tap gesture to watcher one empty bubbles image view
+        let tapGestureRecognizerBubbleWatcherOne = UITapGestureRecognizer(target: self, action: #selector(bubbleWatcherOneTapped(tapGestureRecognizer:)))
+        watcherOneBubbleImageView.addGestureRecognizer(tapGestureRecognizerBubbleWatcherOne)
+        
+        // adding tap gesture to watcher two empty bubbles image view
+        let tapGestureRecognizerBubbleWatcherTwo = UITapGestureRecognizer(target: self, action: #selector(bubbleWatcherTwoTapped(tapGestureRecognizer:)))
+        watcherTwoBubbleImageView.addGestureRecognizer(tapGestureRecognizerBubbleWatcherTwo)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func bubbleWatcherOneTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        //let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        // Your action
+        print("toque la burbuja 1!")
+    }
+    
+    func bubbleWatcherTwoTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+       // let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        // Your action
+        print("toque la burbuja 2!")
+    }
 
 }
 
